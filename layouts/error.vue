34f06e5c19f3227719ebebad67df3012773d0547
Brian Thiel <bthiel@apple.com>
2020-08-24 16:10:35 -0500

    adding error page

---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="error">
+		<h1 v-if="error.statusCode === 404">{{ error.statusCode }}</h1>
+    <h1 v-if="error.statusCode === 404">{{ error.message }}</h1>
+    <h1 v-else>An error occurred</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    layout: 'error' // you can set a custom layout for the error page
+  }
+</script>
+
+<style lang="scss">
+.error {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+}
+</style>
